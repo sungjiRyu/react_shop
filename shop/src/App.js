0f0b0da15c4f01001545910804d18fb4478ba671
ryu <dhftlcm@gmail.com>
2023-12-02 23:06:28 +0900

    이미지 넣기&public폴더 이용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,33 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Navbar, Container, Nav, NavDropdown}  from 'react-bootstrap';
+import bg from './img/bg.png';
 
 function App() {
   return (
     <div className="App">
           <Navbar expand="lg" className="bg-body-tertiary" >
       <Container>
-        <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
+        <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">메뉴1</Nav.Link>
-            <Nav.Link href="#link">메뉴2</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">메뉴1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home">menu</Nav.Link>
+            <Nav.Link href="#link">cart</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    <div className="main-bg" style={{backgroundImage : 'url('+bg+')' }}></div>
+
+    {/* 상품목록 */}
+
+    <div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+      <img src = 'https://codingapple1.github.io/shop/shoes1.jpg' width = "80%"></img>
+      <h4>상품명</h4>
+      <p>상품설명</p>
+    </div>
+    <div class="col">
+      <img src = 'https://codingapple1.github.io/shop/shoes2.jpg' width = "80%"></img>
+      <h4>상품명</h4>
+      <p>상품설명</p>
+    </div>
+    <div class="col">
+      <img src = 'https://codingapple1.github.io/shop/shoes3.jpg' width = "80%"></img>
+      <h4>상품명</h4>
+      <p>상품설명</p>
+    </div>
+   
+  </div>
+</div>
+
     </div>
   );
 }
