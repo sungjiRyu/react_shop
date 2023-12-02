33813ed2db5c05d5f5eda1f264981399276a3b0c
ryu <dhftlcm@gmail.com>
2023-12-03 01:21:33 +0900

    상품 리스트 컴포넌트화

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Navbar, Container, Nav, NavDropdown}  from 'react-bootstrap';
 import bg from './img/bg.png';
+import { useState } from 'react';
+import './App.css';
+import data from './data.js';
+
+
 
 function App() {
+  //상품데이터(원래는 서버에서 받아와야함)
+
+  let [shoes, setShoes] = useState(data);
+
+
   return (
     <div className="App">
-          <Navbar expand="lg" className="bg-body-tertiary" >
+        <Navbar expand="lg" className="bg-body-tertiary" >
       <Container>
         <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -21,33 +30,35 @@ function App() {
     <div className="main-bg" style={{backgroundImage : 'url('+bg+')' }}></div>
 
     {/* 상품목록 */}
-
-    <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
-      <img src = 'https://codingapple1.github.io/shop/shoes1.jpg' width = "80%"></img>
-      <h4>상품명</h4>
-      <p>상품설명</p>
+    <div className="container text-center">
+    <div className="row align-items-start">
+           
+    {
+      shoes.map((a,i)=>{
+        return(
+          <List shoes = {shoes[i]} i = {i} key = {i}></List>
+        )
+      })
+    }
+    
+    
+    
     </div>
-    <div class="col">
-      <img src = 'https://codingapple1.github.io/shop/shoes2.jpg' width = "80%"></img>
-      <h4>상품명</h4>
-      <p>상품설명</p>
     </div>
-    <div class="col">
-      <img src = 'https://codingapple1.github.io/shop/shoes3.jpg' width = "80%"></img>
-      <h4>상품명</h4>
-      <p>상품설명</p>
-    </div>
-   
   </div>
-</div>
-
-    </div>
   );
 }
 
-
+// 상품 목록 컴포넌트
+function List(props){
+    return (
+            <div className="col" >
+              <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="80%" />
+              <h4>{props.shoes.title}</h4>
+              <p>{props.shoes.price}</p>
+        </div>
+    )
+  }
 
 export default App;
 
---
+++ shop/src/data.js
@@ -0,0 +1,34 @@
+// let a = 10;
+// let b = 100;
+
+// // export 후 사용할 컴포넌트에서 import
+// // export default 변수명; 
+// // export default a;
+// // 여러개 export
+// export {a,b}
+
+
+let data = [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
+
+export default data;
