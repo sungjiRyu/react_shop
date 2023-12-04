4256a120b5a7550b309570c24fe710598f34cca1
ryu <dhftlcm@gmail.com>
2023-12-04 22:18:27 +0900

    라우터로 페이지 분할하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,8 +5,9 @@ import bg from './img/bg.png';
 import { useState } from 'react';
 import './App.css';
 import data from './data.js';
-
-
+import { Routes, Route, Link } from 'react-router-dom';
+import main from './components/main.js';
+import detail from './components/detail.js';
 
 function App() {
   //상품데이터(원래는 서버에서 받아와야함)
@@ -15,50 +16,35 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App"> 
+    
+     
+
         <Navbar expand="lg" className="bg-body-tertiary" >
       <Container>
-        <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+        <Navbar.Brand><Link to="/" className='link'>ShoeShop</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Nav className="me-auto">
             <Nav.Link href="#home">menu</Nav.Link>
             <Nav.Link href="#link">cart</Nav.Link>
+            <Nav.Link><Link to="/detail" className='link'>상세페이지</Link></Nav.Link>
           </Nav>
       </Container>
     </Navbar>
 
-    <div className="main-bg" style={{backgroundImage : 'url('+bg+')' }}></div>
+    {/* 라우터로 페이지 나누는 법 */}
+    <Routes>
+        <Route path="/" element={<main bg={bg} shoes={shoes} />}/>
+        <Route path="/detail" element={<detail></detail>}/>
+        <Route/>
+      </Routes>
 
-    {/* 상품목록 */}
-    <div className="container text-center">
-    <div className="row align-items-start">
-           
-    {
-      shoes.map((a,i)=>{
-        return(
-          <List shoes = {shoes[i]} i = {i} key = {i}></List>
-        )
-      })
-    }
     
-    
-    
-    </div>
-    </div>
   </div>
   );
 }
 
-// 상품 목록 컴포넌트
-function List(props){
-    return (
-            <div className="col" >
-              <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="80%" />
-              <h4>{props.shoes.title}</h4>
-              <p>{props.shoes.price}</p>
-        </div>
-    )
-  }
+
 
 export default App;
 
---
+++ shop/src/components/main.js
@@ -0,0 +1,35 @@
+
+import App from 'react';
+
+function main(props){
+    return(
+        
+        <>
+        <div className="main-bg" style={{backgroundImage : 'url('+props.bg+')' }}></div>
+        <div className="container text-center">
+        <div className="row align-items-start">
+          {
+              props.shoes.map((a,i)=>{
+                  return(
+                      <List shoes = {props.shoes[i]} i = {i} key = {i}></List>
+                      )
+                    })
+                }
+          </div>
+          </div>
+          </>
+          )
+        }
+
+// 상품 목록 컴포넌트
+function List(props){
+    return (
+            <div className="col" >
+              <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="80%" />
+              <h4>{props.shoes.title}</h4>
+              <p>{props.shoes.price}</p>
+        </div>
+    )
+  }
+
+export default main;
